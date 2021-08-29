78482c89afdd43d7da1d9a7d4616d901ce79d3a7
ruben6p <85554053+ruben6p@users.noreply.github.com>
2021-08-29 12:54:47 +0200

    prime

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cabecera/cabecera.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.scss']
+})
+export class CabeceraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
